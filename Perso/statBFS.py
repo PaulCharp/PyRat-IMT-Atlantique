e4b928951e0562daf48e5c1a1f8a285f79127a9a
PaulCharp <paulcharp99@gmail.com>
2019-09-27 01:05:27 +0200

    Les stats fonctionnent désormais, on a le temps d'execution en fonction de la taille du labyrinthe.

--- Perso/statBFS.py
+++ Perso/statBFS.py
@@ -1,15 +1,34 @@
 #In this program we'll make a statistic analysis of our BFS algorithm
-#On veut tracer un graphique de l'efficacite de nos algorithmes. Pour ça on trace le temps d'execution divise
-#par le nombre de fromage en fonction du nombre de fromage.
-#Pour chaque nombre de fromage on fait donc une moyenne sur plusieurs experience et on la plot.
-#On doit executer le terminal dans PyRat-master car sinon pyrat ne trouve pas le fichier des ressources
-#synchronous permet de calculer le temps que met notre fonction et enleve les temps parasites
+#On veut tracer un graphique de l'efficacite de nos algorithmes.
+#On va faire varier la dimension de notre labyrinthe. A chaque iteration, on augmente x et y de 1.
+#On a donc une augmentation de l'aire en i^2. On trace donc notre échelle des abscisses en échelle logarithmique
+#en base 2.
 
 import os
+import time
+import matplotlib.pyplot as plt
 
-def plotGraphCheese():
-    for nbCheese in range(1,41):
-        os.system("python pyrat.py --rat ../Perso/BFS.py -p" + str(nbCheese)+" -md 0.0 --nodrawing --synchronous --auto_exit")
+def plotGrapSize():
+    #On recupere les donnes
+    n = 100
+    X = [x**2 for x in range(5,n)]
+    T = []
+    for size in range(5,n):
+        #On fait la moyenne sur 5 valeurs pour lisser la courbe
+        for i in range(5):
+            som = 0
+            t = time.time()
+            os.system("python pyrat.py --rat ../Perso/BFS.py -p 1  -md 0.0 --nodrawing --synchronous --auto_exit -x "+ str(size)+" -y "+str(size))
+            ti = time.time()-t
+            som += ti
+        T.append(som/5)
+    #On trace ensuite notre graphique en fonction de l'aire du labyrinthe
+    plt.plot(X,T)
+    plt.title("Temps d'execution de BFS pour trouver un fromage en fonction de l'aire du labyrinthe.")
+    plt.xlabel("Aire du labyrinthe (en nombre de case au carré)")
+    plt.ylabel("Temps d'execution de l'algorithme (en ms)")
+    plt.show()
+    return
 
 if __name__ == '__main__':
-    plotGraphCheese()
+    plotGrapSize()
